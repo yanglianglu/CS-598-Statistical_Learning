38d0676f919f3a2daaf9bf66ef6b26ca1038a20e
Zeyu Liao <zeyu9@illinois.edu>
2023-12-04 10:46:25 +0800

    without generate myvocab

--- CS_598_Project_3/mymain.py
+++ CS_598_Project_3/mymain.py
@@ -8,10 +8,9 @@ nltk.download('stopwords')
 stopwords = nltk.corpus.stopwords.words('english')
 train = pd.read_csv(r'train.tsv', sep='\t')
 test = pd.read_csv(r'test.tsv', sep='\t')
-test_y = pd.read_csv(r'test_y.tsv', sep='\t')
+
 train_label = train['sentiment']
 train_review =  train['review'].str.replace('&lt;.*?&gt;', ' ', regex=True)
-test_label = test_y['sentiment']
 test_review = test['review'].str.replace('&lt;.*?&gt;', ' ', regex=True)
 
 vocab = pd.read_csv('myvocab.txt', sep='\t')
@@ -43,11 +42,7 @@ def fit_vectorizer(train_review, test_review, vocab):
     train_review = vectorizer.transform(train_review)
     test_review = vectorizer.transform(test_review)
     return train_review, test_review
-
-from sklearn.metrics import roc_auc_score
 from sklearn.linear_model import LogisticRegressionCV
-
-from sklearn.metrics import accuracy_score
 
 train_score, test_score = fit_vectorizer(train_review.copy(), test_review.copy(), vocab)
 model = LogisticRegressionCV(cv=5, max_iter=10000, n_jobs=-1)
